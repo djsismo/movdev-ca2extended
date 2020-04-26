2576604c1a9e993845b7ca0ca2fd207e6f4bc0d8
djsismo <Elias.fuentes04@gmail.com>
2020-04-26 21:00:16 +0000

    added httpclient in app-module.ts

--- mobdev-ca2/src/app/pages/character-details/character-details.page.ts
+++ mobdev-ca2/src/app/pages/character-details/character-details.page.ts
@@ -12,6 +12,10 @@ export class CharacterDetailsPage implements OnInit {
   characterId = null;
   constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
   ngOnInit() {
+    this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.api.getCharacter(this.characterId).subscribe(res => {
+      this.character = res[0];
+      console.log(JSON.stringify(this.character.char_id));
+    });
   }
-
 }
