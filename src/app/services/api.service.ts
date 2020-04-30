33b266bf56c64a0808e44b1694bf3e405bcae6be
djsismo <Elias.fuentes04@gmail.com>
2020-04-30 01:47:42 +0000

    my first Heroku deployment

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+/** api service to return data from the url source*/
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+/**GET Episode from outside source*/
+   getEpisodes() {
+    return this.http.get('https://glacial-earth-68674.herokuapp.com//api/episodes');
+  }
+ /**GET a Episode by ID from outside source*/
+  getEpisode(id) {
+    return this.http.get(`https://glacial-earth-68674.herokuapp.com//api/episodes/${id}`);
+  }
+/**GET character from outside source*/
+  getCharacters() {
+        return this.http.get('https://glacial-earth-68674.herokuapp.com//api/characters');
+    }
+/**GET character by ID from outside source*/
+    getCharacter(id) {
+        return this.http.get(`https://glacial-earth-68674.herokuapp.com//api/characters/${id}`);
+    }
+    
+    /**GET quotes reletad to episodes from outside source*/
+    getQuotes() {
+        return this.http.get('https://glacial-earth-68674.herokuapp.com//api/quotes');
+    }
+    /**GET quotes by ID reletad to episodes from outside source*/
+    getQuote(id) {
+        return this.http.get(`https://glacial-earth-68674.herokuapp.com//api/quotes/${id}`);
+    }
+/**GET number of deaths reletad to a character from outside source*/
+    getDeaths() {
+        return this.http.get('https://glacial-earth-68674.herokuapp.com//api/deaths');
+    }
+    getDeath(id) {
+        return this.http.get('https://glacial-earth-68674.herokuapp.com//api/deaths');
+    }
+
+}
